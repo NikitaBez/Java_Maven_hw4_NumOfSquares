13c2ec3a62aeb2522c55088e79cdffaea860ddc7
NikitaBez <bezmenov.nikita@gmail.com>
2022-07-31 00:25:30 +0900

    Deleted target. Changed parameter names

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -2,12 +2,12 @@ package ru.netology.sqr;
 
 public class SQRService {
 
-    public int numberOfSquares (int num10, int num99) {
+    public int numberOfSquares (int min, int max) {
         int counter = 0;
         for (int i = 10; i <= 99; i++) {
             int sqr = i * i;
-            if (sqr >= num10) {
-                if (sqr <= num99) {
+            if (sqr >= min) {
+                if (sqr <= max) {
                     counter++;
                 }
             }
